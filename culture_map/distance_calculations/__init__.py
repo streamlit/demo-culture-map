ad514190b4938393d77d0eb27c40a5da2c611ee8
mnowotka <michal.nowotka@snowflake.com>
2022-12-13 09:47:58 +0100

    Choosing distance kinds.

--- culture_map/distance_calculations/__init__.py
+++ culture_map/distance_calculations/__init__.py
@@ -1,34 +1,50 @@
 import typing
 
 import pandas as pd
+from scipy.spatial import distance
 import streamlit as st
 
 from culture_map.country_data import types
 
 HOFSTEDE_DIMENSIONS = ['pdi', 'idv', 'mas', 'uai', 'lto', 'ind', 'ivr']
+AVAILABLE_DISTANCES = {
+    "Euclidean": distance.euclidean,
+    "Cosine": distance.cosine,
+    "Manhattan": distance.cityblock,
+    "Correlation": distance.correlation
+
+}
 TO_PERCENT = 100.0
 SQUARE = 2
 PandasDataFrame = typing.TypeVar('pandas.core.frame.DataFrame')
 
 
-def compute_distance(country_from: types.CountryInfo, country_to: types.CountryInfo) -> float:
-    return sum(
-        [
-            (int(getattr(country_from, dimension)) - int(getattr(country_to, dimension))) ** SQUARE
-            for dimension in HOFSTEDE_DIMENSIONS
-        ]
-    )
+def compute_dimensions(countries: types.Countries) -> PandasDataFrame:
+    index = [country.title for country in countries]
+    dimensions = {}
+    for dimension in HOFSTEDE_DIMENSIONS:
+        row = []
+        for country in countries:
+            row.append(getattr(country, dimension))
+        dimensions[dimension] = row
+    return pd.DataFrame(dimensions, index=index)
+
+
+def compute_distance(country_from: types.CountryInfo, country_to: types.CountryInfo, distance_metric: str) -> float:
+    from_array = [max(getattr(country_from, dimension, 0), 0) for dimension in HOFSTEDE_DIMENSIONS]
+    to_array = [max(getattr(country_to, dimension, 0), 0) for dimension in HOFSTEDE_DIMENSIONS]
+    return AVAILABLE_DISTANCES[distance_metric](from_array, to_array)
 
 
 @st.cache
-def compute_distances(countries: types.Countries) -> tuple[PandasDataFrame, float]:
+def compute_distances(countries: types.Countries, distance_metric: str) -> tuple[PandasDataFrame, float]:
     index = [country.title for country in countries]
     distances = {}
     max_distance = 0
     for country_from in countries:
         row = []
         for country_to in countries:
-            distance = compute_distance(country_from, country_to)
+            distance = compute_distance(country_from, country_to, distance_metric)
             max_distance = max(max_distance, distance)
             row.append(distance)
         distances[country_from.title] = row
--- culture_map/scripts/app.py
+++ culture_map/scripts/app.py
@@ -1,12 +1,13 @@
 import random
 
 import streamlit as st
+import matplotlib.pyplot as plt
 
 from culture_map import country_data
 from culture_map import distance_calculations
 from culture_map import visualisation
 
-DEFAULT_COUNTRY_NUMBER = 4
+DEFAULT_COUNTRY_NUMBER = 10
 
 all_countries = country_data.get_country_dict()
 all_countries_names = list(all_countries.keys())
@@ -28,11 +29,20 @@ st.write('You selected:', selected_countries_names)
 
 selected_countries = [country for country in all_countries.values() if country.title in selected_countries_names]
 
-distances, max_distance = distance_calculations.compute_distances(selected_countries)
+distance_metric = st.selectbox('What distance metric would you like to use?',
+                               list(distance_calculations.AVAILABLE_DISTANCES.keys()))
+
+distances, max_distance = distance_calculations.compute_distances(selected_countries, distance_metric)
 st.write('Country distances before normalisation:', distances)
 
 normalised_distances = distance_calculations.normalise_distance_matrix(distances, max_distance)
 st.write('Country distances after normalisation:', normalised_distances)
+
+dimensions = distance_calculations.compute_dimensions(selected_countries)
+st.write('Dimensions:', dimensions.transpose())
+fig, ax = plt.subplots(figsize=(15, 15))
+dimensions.plot.bar(ax=ax)
+st.pyplot(fig)
 
 show_clusters = st.checkbox('Show clusters', value=True)
 heatmap = visualisation.generate_heatmap(normalised_distances, show_clusters)
